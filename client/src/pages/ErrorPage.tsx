842de9c0cc382922368b8f6ecdf819a48aa12b80
LawrenceLevel-FS <levelhlawrence@gmail.com>
2024-10-13 22:26:15 -0400

    setting up frontend router

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <section>
+      <h1>Opps 404 Page not found</h1>
+    </section>
+  );
+};
+
+export default ErrorPage;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section>
+      <h1 className="text-red-500">Hello Home Page</h1>
+    </section>
+  );
+};
+
+export default Home;
